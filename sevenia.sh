7d14f050c07036b20317566e8f3e2b9f09308ee7
Syn7ia <syn7ia@gmail.com>
2016-11-05 02:49:28 +0700

    Create sevenia.sh

---
+++ sevenia.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+# ---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+#
+# BASH SEVENIA
+#
+# Sevenia's Bash template
+#
+# Name          : sevenia.sh
+# Location      : ./
+# Dependency    : which, whoami
+# Programmer(s) : SynSevenia
+# Last Edited   : 2016-11-05 02:48
+#
+# ---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+
+# BEGIN
+
+error_code=0
+user1=root
+user2=pi
+
+_whoami=$(which whoami)
+# echo _whoami=$_whoami
+
+script_user=$($_whoami)
+# echo script_user=\"$script_user\"
+
+if [ z$script_user == z$user1 -o z$script_user == z$user2 ]; then
+   echo "User is able to execute this script" > /dev/null
+else
+   error_code=10
+   echo -n "ERROR($error_code): "
+   echo "Only 'root' or 'pi' can execute this script!"
+   echo " "
+   exit $error_code
+fi
+
+echo "Hello friend"
+
+# END
